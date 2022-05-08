6c334b31841b90cf03262b6ff97fed41e57300ea
Brad <github@bnjf.id.au>
2022-05-08 00:54:14 +0000

    pp

---
+++ OpNodePrinter.py
@@ -0,0 +1,27 @@
+
+class OpNodePrinter:
+    def __init__(self, val):
+        self.val = val
+
+    def to_string (self):
+        op = self.val['op']
+        value = self.val['value']
+        left = self.val['left']
+        right = self.val['right']
+
+        op_to_str = [ "#", "x", "%ptr", "sub", "add", "xor", "mul", "rol", "ror", "shl", "shr", "or", "and_", "imul", "jnz" ]
+
+        if op < 3:
+            return hex(value)
+        else:
+            l = left.dereference()
+            r = right.dereference()
+            return f'{op_to_str[op]}({l}, {r})'
+
+def lookup_type (val):
+    if str(val.type) == 'op_node_t':
+        return OpNodePrinter(val)
+    return None
+
+gdb.pretty_printers.append (lookup_type)
+
