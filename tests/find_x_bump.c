597a77a43fb129e326480336bcdc0f11596eca0e
Brad <github@bnjf.id.au>
2022-05-21 08:49:22 +0000

    solution tester

--- tests/find_x_bump.c
+++ tests/find_x_bump.c
@@ -8,36 +8,61 @@
 
 #include "find_x_bump.h"
 
+static const char *const op_to_str[] = {
+    [OPERAND_IMM] = "#", [OPERAND_TARGET] = "x", [OPERAND_PTR] = "%ptr",
+    [OP_SUB] = "SUB",    [OP_ADD] = "ADD",       [OP_XOR] = "XOR",
+    [OP_MUL] = "MUL",    [OP_ROL] = "ROL",       [OP_ROR] = "ROR",
+    [OP_SHL] = "SHL",    [OP_SHR] = "SHR",       [OP_OR] = "OR",
+    [OP_AND] = "AND",    [OP_IMUL] = "IMUL",     [OP_JNZ] = "JNZ"};
+
+void print_ops_tree(const op_node_t *t, const int d) {
+  const char *const tree_chars =
+      "+-------------------------------------------------------------";
+
+  if (is_operand(t)) {
+    printf("%.*s %s %x\n", (d + 1) * 2, tree_chars, op_to_str[t->op],
+           t->operand);
+    return;
+  }
+
+  printf("%.*s %s\n", (d + 1) * 2, tree_chars, op_to_str[t->op]);
+  print_ops_tree(t->left, d + 1);
+  print_ops_tree(t->right, d + 1);
+  return;
+}
+
 int main(int argc, char *argv[]) {
   uint32_t x = 1;
   op_node_t *t, *t_x, *tinv, *tinv_x;
-  const int NUM_TESTS = 100000;
+  const int NUM_TESTS = atoi(argv[1]);
+  const int JUNK_NUM = atoi(argv[2]);
+  const int JUNK_FLAG = ((1 << JUNK_NUM) - 1);
+  const int NODES = 2 + (JUNK_FLAG * 2) - atoi(argv[3]);
 
+  D("%u %u %u %u %lu\n", NUM_TESTS, JUNK_NUM, JUNK_FLAG, NODES,
+    NODES * sizeof(op_node_t));
   for (int i = 0; i < NUM_TESTS; i++) {
     rnd_init(x++);
 
-    op_node_t *const t0 = (op_node_t *)calloc(0x21, sizeof(op_node_t));
-    t = t0;
-    t_x = make_ops_tree(t, 0xf, 0);
+    op_node_t *const t0 = (op_node_t *)calloc(NODES, sizeof(op_node_t));
+
+    t_x = make_ops_tree(t0, JUNK_FLAG, 0);
     tinv_x = invert_ops_tree(t0, t_x);
+
     // empty
-    if (tinv_x == NULL)
+    if (tinv_x == NULL) {
+      free(t0);
       continue;
-    // printf("%u\t%u,%u\n", tinv_x->op, tinv_x->left->op, tinv_x->right->op);
+    }
+    // printf("%u\t%u,%u\n", tinv_x->op, tinv_x->left->op,
+    // tinv_x->right->op);
 
     if (tinv_x->op == OP_ADD || tinv_x->op == OP_SUB) {
-      uint32_t a = tinv_x->left->op;
-      uint32_t b = tinv_x->right->op;
-      // printf("%u %u %u %u\n", tinv_x->op, a, b,
-      // -adjust_ptr_operand(tinv_x));
       printf("%s\n",
              adjust_ptr_operand(tinv_x) ? "adjusted" : "not_adjusted");
     } else {
       printf("not_add_or_sub\n");
     }
-    // adjust_ptr_operand(tinv_x);
-    // printf("[%u] %u\t%u,%u => %u\t%u,%u\r", i, tinv_x->op, a, b,
-    // tinv_x->op, tinv_x->left->operand, tinv_x->right->operand);
     free(t0);
   }
   exit(0);
--- tests/mock/rnd.c
+++ tests/mock/rnd.c
@@ -6,7 +6,7 @@
 static uint16_t rnd_x;
 static uint16_t rnd_c;
 
-long rnd_n(int n) {
+long rnd_n(const int n) {
   uint16_t x, r;
   do {
     x = rnd();
@@ -15,14 +15,14 @@ long rnd_n(int n) {
   return r;
 }
 
-void rnd_init(uint32_t x) {
-  rnd_x = x;
+void rnd_init(const uint32_t x) {
+  rnd_x = x & 0xffff;
   rnd_c = x >> 16;
 }
 
 long rnd() {
   uint32_t t = 0xfea0UL * rnd_x + rnd_c;
-  rnd_x = t;
+  rnd_x = t & 0xffff;
   rnd_c = t >> 16;
   return rnd_x;
 }
